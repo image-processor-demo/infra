5f80a8af0964899416cac231c5795290af7eac03
samimassoud <samimassoud67@gmail.com>
2026-01-04 16:39:36 +0200

    Chore: fixed wiring between modules via variables

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,23 +1,18 @@
 module "backend" {
-  source = "git::https://github.com/image-processor-demo/modules.git//backend?ref=v0.2.1"
-
-  environment = var.environment
-  aws_region  = var.aws_region
-
+  source                = "git::https://github.com/image-processor-demo/modules.git//backend?ref=v0.2.1"
+  environment           = var.environment
+  aws_region            = var.aws_region
   artifacts_bucket_name = data.terraform_remote_state.global.outputs.artifacts_bucket_name
   artifact_key = yamldecode(
     file("${path.module}/versions.yml")
   ).backend.lambda
-
   api_shared_secret = var.api_shared_secret
 }
 
 module "frontend" {
-  source = "git::https://github.com/image-processor-demo/modules.git//frontend?ref=v0.2.1"
-
-  environment       = var.environment
-  api_shared_secret = var.api_shared_secret
-
+  source                  = "git::https://github.com/image-processor-demo/modules.git//frontend?ref=v0.2.1"
+  environment             = var.environment
+  api_shared_secret       = var.api_shared_secret
   api_gateway_domain_name = module.backend.api_gateway_domain_name
   api_gateway_stage_name  = module.backend.api_gateway_stage_name
 }
