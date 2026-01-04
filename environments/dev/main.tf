45a99413d84c8dd9ae9b192ea2649a5a80f1ad27
samimassoud <samimassoud67@gmail.com>
2026-01-04 16:55:17 +0200

    Hotfix: referencing the lambda version correctly

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,16 +1,16 @@
 module "backend" {
-  source                = "git::https://github.com/image-processor-demo/modules.git//backend?ref=v0.2.2"
+  source                = "git::https://github.com/image-processor-demo/modules.git//backend?ref=v0.2.4"
   environment           = var.environment
   aws_region            = var.aws_region
   artifacts_bucket_name = data.terraform_remote_state.global.outputs.artifacts_bucket_name
   artifact_key = yamldecode(
     file("${path.module}/versions.yml")
-  ).backend.lambda
+  ).backend.lambda.process
   api_shared_secret = var.api_shared_secret
 }
 
 module "frontend" {
-  source                  = "git::https://github.com/image-processor-demo/modules.git//frontend?ref=v0.2.2"
+  source                  = "git::https://github.com/image-processor-demo/modules.git//frontend?ref=v0.2.4"
   environment             = var.environment
   api_shared_secret       = var.api_shared_secret
   api_gateway_domain_name = module.backend.api_gateway_domain_name
