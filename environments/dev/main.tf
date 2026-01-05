190cc7ad14285ffbb15da0305b8318cfa03c6502
samimassoud <samimassoud67@gmail.com>
2026-01-05 16:47:14 +0200

    R

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "backend" {
-  source                = "git::https://github.com/image-processor-demo/modules.git//backend?ref=v0.3.2"
+  source                = "git::https://github.com/image-processor-demo/modules.git//backend?ref=v0.3.3"
   environment           = var.environment
   aws_region            = var.aws_region
   artifacts_bucket_name = data.terraform_remote_state.global.outputs.artifacts_bucket_name
@@ -10,7 +10,7 @@ module "backend" {
 }
 
 module "frontend" {
-  source                  = "git::https://github.com/image-processor-demo/modules.git//frontend?ref=v0.3.2"
+  source                  = "git::https://github.com/image-processor-demo/modules.git//frontend?ref=v0.3.3"
   environment             = var.environment
   api_shared_secret       = var.api_shared_secret
   api_gateway_domain_name = module.backend.api_gateway_domain_name
